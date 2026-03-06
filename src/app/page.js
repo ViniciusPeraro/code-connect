f2b2d112b58d13447d47e084ebe825d9dd078369
Vinicius Peraro <viniciusperaro@outlook.com.br>
2026-03-06 14:17:17 -0300

    fix: import errors

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,51 @@
+import logger from "@/logger";
+import styles from "./page.module.css";
+import { CardPost } from "@/components/CardPost";
+import Link from "next/link";
+
 export default async function Home({ searchParams }) {
+  async function getAllPosts(page, searchQuery) {
+    try {
+      const where = {};
+
+      if (searchQuery) {
+        where.title = {
+          contains: searchQuery,
+          mode: "insensitive",
+        };
+      }
+
+      console.log(
+        "Obtendo posts para a página:",
+        page,
+        "com busca:",
+        searchQuery,
+      );
+      const perPage = 6;
+      const skip = (page - 1) * perPage;
+      const totalPosts = await db.post.count({ where });
+      const totalPages = Math.ceil(totalPosts / perPage);
+
+      const prev = page > 1 ? page - 1 : null;
+      const next = page < totalPages ? page + 1 : null;
+
+      const posts = await db.post.findMany({
+        take: perPage,
+        skip: skip,
+        where,
+        orderBy: {
+          createdAt: "desc",
+        },
+        include: {
+          author: true,
+        },
+      });
+      return { data: posts, prev, next };
+    } catch (error) {
+      logger.error("Erro ao obter os posts: " + error.message);
+      return { data: [], prev: null, next: null };
+    }
+  }
   const pageParam = Array.isArray(searchParams?.page)
     ? searchParams.page[0]
     : searchParams?.page;
@@ -9,12 +56,16 @@ export default async function Home({ searchParams }) {
     ? searchParams.q[0]
     : searchParams?.q || "";
 
-  const { data: posts = [], prev, next } = await getAllPosts(currentPage, searchQuery);
+  const {
+    data: posts = [],
+    prev,
+    next,
+  } = await getAllPosts(currentPage, searchQuery);
 
   return (
     <main className={styles.grid}>
-      {posts?.map(post => (
-        <CardPost key={post.id} post={post}/>
+      {posts?.map((post) => (
+        <CardPost key={post.id} post={post} />
       ))}
 
       <div className={styles.pagination}>

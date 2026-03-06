7b73a75aeb0b87399b17fd8d5a910a7180759d71
Vinicius Peraro <viniciusperaro@outlook.com.br>
2026-03-06 14:00:04 -0300

    modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,55 +1,34 @@
-import logger from "@/logger";
-import { CardPost } from "../components/CardPost";
-import styles from "./page.module.css";
-import Link from "next/link";
-import db from "../../prisma/db";
-
-async function getAllPosts (page, searchQuery) {
-  try {
-    const where = {}
-
-    if (searchQuery) {
-      where.title = { 
-        contains: searchQuery, mode: 'insensitive' };
-    }
-
-    console.log('Obtendo posts para a página:', page, 'com busca:', searchQuery);
-    const perPage = 6;
-    const skip = (page - 1) * perPage;
-    const totalPosts = await db.post.count({ where });
-    const totalPages = Math.ceil(totalPosts / perPage);
-
-    const prev = page > 1 ? page - 1 : null;
-    const next = page < totalPages ? page + 1 : null;
-
-    const posts = await db.post.findMany({
-      take: perPage,
-      skip: skip,
-      where,
-      orderBy: {
-        createdAt: 'desc'
-      },
-      include: {
-        author: true
-      }
-    });
-    return { data: posts, prev, next };
-  } catch (error) {
-    logger.error('Erro ao obter os posts: ' + error.message);
-    return { data: [], prev: null, next: null };
-  }
-}
-
 export default async function Home({ searchParams }) {
-  const currentPage = parseInt(searchParams?.page || 1);
-  const searchQuery = searchParams?.q || '';
-  const { data: posts, prev, next} = await getAllPosts(currentPage, searchQuery);
+  const pageParam = Array.isArray(searchParams?.page)
+    ? searchParams.page[0]
+    : searchParams?.page;
+
+  const currentPage = parseInt(pageParam || "1", 10);
+
+  const searchQuery = Array.isArray(searchParams?.q)
+    ? searchParams.q[0]
+    : searchParams?.q || "";
+
+  const { data: posts = [], prev, next } = await getAllPosts(currentPage, searchQuery);
+
   return (
     <main className={styles.grid}>
-      {posts.map(post => <CardPost key={post.id} post={post}/>)}
+      {posts?.map(post => (
+        <CardPost key={post.id} post={post}/>
+      ))}
+
       <div className={styles.pagination}>
-        {prev && <Link href={{pathname: '/', query: {page: prev, q: searchQuery}}}>Página Anterior</Link>}
-        {next && <Link href={{pathname: '/', query: {page: next, q: searchQuery}}}>Próxima Página</Link>}
+        {prev && (
+          <Link href={{ pathname: "/", query: { page: prev, q: searchQuery } }}>
+            Página Anterior
+          </Link>
+        )}
+
+        {next && (
+          <Link href={{ pathname: "/", query: { page: next, q: searchQuery } }}>
+            Próxima Página
+          </Link>
+        )}
       </div>
     </main>
   );
